c0a6cdcd1cbdc3f42b8341b3ff59348499df9de3
Joseph Knowles <joejknowles@gmail.com>
2017-02-25 18:36:39 +0000

    implement react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import Events from './components/events';
+import Routes from './components/routes';
 
 export default () => (
   <div className="App">
     <div className="App-header">
       <h2>EG Tech Test</h2>
     </div>
-    <Events />
+    <Routes />
   </div>
 );
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Router, Route, browserHistory } from 'react-router';
+import Events from './events';
 
 export default (props) => (
-    <div>
-    </div>
+    <Router history={browserHistory} >
+      <Route path="/" component={ Events } />
+    </Router>
 );
