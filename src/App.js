b3ef22b97dedf3bc082886c835bd7262a2d44aad
Joseph Knowles <joejknowles@gmail.com>
2017-02-25 12:20:29 +0000

    display event titles

--- src/App.js
+++ src/App.js
@@ -21,11 +21,13 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>eg Tech Test</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul className="App-intro">
+          { this.state.events.map((event) => (
+            <li key={ event.id }>{ event.title }</li>
+          )) }
+        </ul>
       </div>
     );
   }
