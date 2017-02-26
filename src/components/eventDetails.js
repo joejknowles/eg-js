4015b5732de060a473f88f14b50f426acc5e56f7
Joseph Knowles <joejknowles@gmail.com>
2017-02-26 11:05:06 +0000

    convert EventDetails component to class

--- src/components/eventDetails.js
+++ src/components/eventDetails.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { createEventSelector } from '../reducers'
 
-export const EventDetails = (props) => (
-  <div key={ props.id } className="event-details">
-    <img src={ props.image } className="event-image" alt="event" />
-    <h3 className="event-title">{ props.title }</h3>
-    <p>
-      { props.date }, at { props.venue }
-    </p>
-  </div>
-);
+export class EventDetails extends Component {
+  render() {
+    const props = this.props;
+    return (
+     <div key={ props.id } className="event-details">
+       <img src={ props.image } className="event-image" alt="event" />
+       <h3 className="event-title">{ props.title }</h3>
+       <p>
+         { props.date }, at { props.venue }
+       </p>
+     </div>
+   );
+  }
+}
+
+
 
 const mapStateToProps = (state, { params: { id } }) => (
   createEventSelector(id)
