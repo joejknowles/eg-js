fd7380385b72b5e090874b28e0acb1506d94169b
Joseph Knowles <joejknowles@gmail.com>
2017-02-26 10:45:19 +0000

    test componentDidMount on Events component

--- src/tests/components/events.test.js
+++ src/tests/components/events.test.js
@@ -2,10 +2,25 @@ import { Events } from '../../components/events';
 
 import React from 'react';
 import ReactTestUtils from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
 
 const shallowRenderer = ReactTestUtils.createRenderer();
 
-it('renders events as before', () => {
-  const component = shallowRenderer.render(<Events events={ [] } />);
-  expect(component).toMatchSnapshot();
+describe('Events Component', () => {
+  it('renders as before', () => {
+    const component = shallowRenderer.render(<Events events={ [] } />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('calls fetchEvents on componentDidMount', () => {
+    const fetchEventsMock = jest.fn();
+    const component = shallow(
+      <Events
+        events={ [] }
+        fetchEvents={ fetchEventsMock }
+      />
+    );
+    component.instance().componentDidMount();
+    expect(fetchEventsMock).toHaveBeenCalled();
+  });
 });
