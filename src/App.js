589404d0f55b359f154bdb72b24afb2bbdbf44d1
Joseph Knowles <joejknowles@gmail.com>
2017-02-26 19:33:49 +0000

    add link to home page on header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router';
 
 import Routes from './components/routes';
 import configureStore from './configureStore';
@@ -8,7 +9,9 @@ import configureStore from './configureStore';
 export default () => (
   <div className="App">
     <div className="App-header">
-      <h2>EG Tech Test</h2>
+      <h2><a onClick={ () => browserHistory.push('/') } style={ { cursor: 'pointer' } }>
+        EG Tech Test
+      </a></h2>
     </div>
     <div className="content">
       <Provider store={ configureStore() } >
