3a611a935992d9a7ec962cc60c189fc53831764d
Joseph Knowles <joejknowles@gmail.com>
2017-02-26 10:25:18 +0000

    add event detail route

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
 import Events from './events';
+import EventDetails from './eventDetails';
 import NotFound from './404';
 
 export default () => (
     <Router history={browserHistory} >
       <Route path="/" component={ Events } />
+      <Route path="/event/:id" component={ EventDetails } />
       <Route path="*" component={ NotFound } />
     </Router>
 );
