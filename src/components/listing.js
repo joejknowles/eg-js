d2cf6e7fcefe1e36c5545634da1ef6a604496815
Joseph Knowles <joejknowles@gmail.com>
2017-02-26 10:33:36 +0000

    add event details link

--- src/components/listing.js
+++ src/components/listing.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 import { createEventSelector } from '../reducers'
 
 export const Listing = (props) => (
-    <div key={ props.id } className="event-listing">
+  <div key={ props.id } className="event-listing">
     <img src={ props.image } className="event-image" alt="event" />
+    <Link to={ `event/${ props.id }` }>
       <h3 className="event-title">{ props.title }</h3>
-      <p>
-        { props.date }, at { props.venue }
-      </p>
-    </div>
+    </Link>
+    <p>
+      { props.date }, at { props.venue }
+    </p>
+  </div>
 );
-
 
 const mapStateToProps = (state, { id }) => (
   createEventSelector(id)
