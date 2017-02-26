9428961c7074ffdd936f2a9ac862594e6d84873f
Joseph Knowles <joejknowles@gmail.com>
2017-02-26 11:42:18 +0000

    add fetchEventDetails sagas

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,6 @@
 import { call, takeEvery, put } from 'redux-saga/effects';
 import * as apiClients from '../apiClients';
-import { fetchEventsSuccess } from '../actions';
+import { fetchEventsSuccess, fetchEventDetailsSuccess } from '../actions';
 
 export function* fetchEvents() {
   const response = yield call(apiClients.fetchEvents);
@@ -11,8 +11,18 @@ export function* watchEventsRequests() {
   yield takeEvery('FETCH_EVENTS_REQUEST', fetchEvents);
 }
 
+export function* fetchEventDetails() {
+  const response = yield call(apiClients.fetchEvents);
+  yield put(fetchEventDetailsSuccess(response));
+}
+
+export function* watchEventDetailsRequests() {
+  yield takeEvery('FETCH_EVENT_DETAILS_REQUEST', fetchEventDetails);
+}
+
 export default function* rootSaga() {
   yield [
-    watchEventsRequests()
+    watchEventsRequests(),
+    watchEventDetailsRequests()
   ];
 }
