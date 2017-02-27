264ed659e9055dca428e8694f97d8a38ce77dbef
Joseph Knowles <joejknowles@gmail.com>
2017-02-27 11:46:57 +0000

    add filter components to filters

--- src/components/events/filters/index.js
+++ src/components/events/filters/index.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import throttle from 'lodash.throttle';
 
-import { TypeFilter } from './type'
+import { FilterDropDown } from './dropDown'
+
+const typeOptions = [
+  'gig',
+  'other'
+];
+
+const locationOptions = [
+  'Manchester',
+  'Ibiza'
+];
 
 export const Filters = (props) => (
   <div className='Filters'>
-    <TypeFilter onChange={ props.onTypeFilterChange }/>
+    <FilterDropDown
+      name="type"
+      options={ typeOptions }
+      onChange={ props.onTypeFilterChange }/>
+    <FilterDropDown
+      name="location"
+      options={ locationOptions }
+      onChange={ props.onLocationFilterChange }/>
+    <input placeholder="search..."
+      onChange={ (e) => {
+        const value = e.target.value;
+        value.length > 2 && props.onTitleSearch(value);
+      } }
+    />
   </div>
 );
 
 const mapStateToDispatch = (dispatch) => ({
-  onTypeFilterChange: (filter) => dispatch({ type: 'SET_TYPE_FILTER', filter})
+  onTypeFilterChange: (filter) => dispatch({ type: 'SET_TYPE_FILTER', filter}),
+  onLocationFilterChange: (filter) => dispatch({ type: 'SET_LOCATION_FILTER', filter}),
+  onTitleSearch: (term) => throttle(dispatch({ type: 'SET_TITLE_SEARCH', term}))
 });
 
 
